cdacf05022b46c26918b2d7945292e06ef3b425e
Sơn Nguyễn <54360939+zenson22@users.noreply.github.com>
2022-03-11 20:18:41 +0700

    Create DB
    - fix no table

--- src/main/java/com/sonnguyen/webdochoioto/config/JPAConfig.java
+++ src/main/java/com/sonnguyen/webdochoioto/config/JPAConfig.java
@@ -70,7 +70,7 @@ public class JPAConfig {
 	Properties additionalProperties() {
 		Properties properties = new Properties();
 		//properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
-		properties.setProperty("hibernate.hbm2ddl.auto", "update");
+		properties.setProperty("hibernate.hbm2ddl.auto", "create");
 		//properties.setProperty("hibernate.hbm2ddl.auto", "none");
 		properties.setProperty("hibernate.show_sql", "true");
 		properties.setProperty("hibernate.format_sql", "true");
